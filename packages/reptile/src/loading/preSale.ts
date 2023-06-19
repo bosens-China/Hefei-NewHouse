b114c961544f73ab0114378d77a191eaefeb05bb
Yliu <yangboses@gmail.com>
2023-06-19 22:04:02 +0800

    fix: 修复爬取预售覆盖current问题

--- packages/reptile/src/loading/preSale.ts
+++ packages/reptile/src/loading/preSale.ts
@@ -2,12 +2,13 @@ import { getPreSale, mergeArrays, consolidationResultPreSale } from '../reptile/
 import { NUMBER_OF_PRE_SALE_COLUMNS } from '../constant';
 import dayjs from 'dayjs';
 import { preSaleDb } from '@new-house/data/preSale';
+import _ from 'lodash-es';
 
 // 加载预售
 export const loadingPreSale = async () => {
   preSaleDb.read();
   const { values, total } = await getPreSale();
-  const { total: currentTotal = 0 } = preSaleDb.data;
+  const { total: currentTotal = 0, current } = preSaleDb.data;
   let page = 1;
   if (currentTotal) {
     page = Math.ceil((total - currentTotal) / NUMBER_OF_PRE_SALE_COLUMNS) + 1;
@@ -18,22 +19,24 @@ export const loadingPreSale = async () => {
     values.push(...result.values);
   }
 
-  const current = dayjs();
-
   // 只返回今天和之后的
-  const result = mergeArrays(
+  const filteringValue = mergeArrays(
     await Promise.all(
       (currentTotal ? values.slice(0, total - currentTotal) : values)
         .filter((f) => {
-          return dayjs(current).isSameOrBefore(dayjs(f.releaseDate), 'D');
+          return dayjs().isSameOrBefore(dayjs(f.releaseDate), 'D');
         })
         .map(async (item) => {
           return await consolidationResultPreSale(item);
         }),
     ),
   );
+  const realResults = _.uniqBy([...filteringValue, ...current], (item) => {
+    return `${item.entryName}${item.buildingNumber.join(',')}`;
+  });
+
   preSaleDb.data.total = total;
-  preSaleDb.data.current = result;
+  preSaleDb.data.current = realResults;
   preSaleDb.write();
-  return result;
+  return currentTotal !== total ? realResults : [];
 };
